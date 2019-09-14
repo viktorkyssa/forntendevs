76ea90c2130cb401ac28d84468dd87965c3d8ea7
Viktor Kyssa <vkibank@gmail.com>
2019-09-14 14:21:40 +0200

    Updates

---
+++ precache-manifest.52a486bc7badb7ddc9b24ba9a3745839.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a3eeb00453f3f0ca5b9432e37bf439",
+    "url": "/forntendevs/index.html"
+  },
+  {
+    "revision": "c85045febd22fbbd1bf3",
+    "url": "/forntendevs/static/css/3.7893d4f7.chunk.css"
+  },
+  {
+    "revision": "24cdc5d6cc409b6d64fd",
+    "url": "/forntendevs/static/css/4.bb6d6209.chunk.css"
+  },
+  {
+    "revision": "e2b17fed9182bf1173df",
+    "url": "/forntendevs/static/css/main.ba7f617d.chunk.css"
+  },
+  {
+    "revision": "0cf589ce28ef786e1ea5",
+    "url": "/forntendevs/static/js/2.81feda70.chunk.js"
+  },
+  {
+    "revision": "c85045febd22fbbd1bf3",
+    "url": "/forntendevs/static/js/3.b60b8676.chunk.js"
+  },
+  {
+    "revision": "24cdc5d6cc409b6d64fd",
+    "url": "/forntendevs/static/js/4.15e7b266.chunk.js"
+  },
+  {
+    "revision": "e2b17fed9182bf1173df",
+    "url": "/forntendevs/static/js/main.1e77bd34.chunk.js"
+  },
+  {
+    "revision": "6e903f21d524110774a2",
+    "url": "/forntendevs/static/js/runtime~main.efdd44f1.js"
+  },
+  {
+    "revision": "630c3d9321f7e240bb29e0cb912930b1",
+    "url": "/forntendevs/static/media/loading.630c3d93.gif"
+  },
+  {
+    "revision": "aa960f01e239928b79aee6f73cb508ab",
+    "url": "/forntendevs/static/media/user.aa960f01.png"
+  }
+]);
