5e94d885dcb163c7aa0f08e4adc34cfb6f105bb3
viktorkissa <vkibank@gmail.com>
2020-02-27 16:18:10 +0200

    MyPostsContainer with TS

---
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+import {addPost} from "../../../redux/profile-reducer"
+import {PostType} from "../../../types/types"
+import {AppStateType} from "../../../redux/redux-store"
+
+import MyPosts from './MyPosts'
+
+type MapStatePropsType = {
+    posts: Array<PostType>
+}
+
+type MapDispatchPropsType = {
+    addPost: (newPostText: string) => void
+}
+
+let mapStateToProps = (state: AppStateType): MapStatePropsType => ({
+    posts: state.profilePage.posts
+})
+
+const MyPostsContainer = connect<MapStatePropsType, MapDispatchPropsType, unknown, AppStateType>
+        (mapStateToProps,{addPost})(MyPosts)
+
+export default MyPostsContainer
